8d2cb83486be0f9773af5b5359ce7b0fc80e2572
Adhiswauran V <adhiswauranv@gmail.com>
2026-07-24 13:31:09 +0530

    Repo cleanup: remove stale migration patch script, archived broken migrations, and debug scratch files

--- backend/fix_alembic.py
+++
@@ -1,7 +0,0 @@
-from database import engine
-from sqlalchemy import text
-
-conn = engine.connect()
-conn.execute(text("UPDATE alembic_version SET version_num = '765609b6549f'"))
-conn.commit()
-print("Fixed")
